0789c054a224588df3621c4c23320ed70e25451c
jassmanni <jassmanniq@gmail.com>
2021-04-16 09:59:09 -0400

    added firebase.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig = {
+    apiKey: 'AIzaSyAWHCCh3ywXTvUIMWSx_j2F58Ah0zl7Wuw',
+    authDomain: 'storeapp-9941c.firebaseapp.com',
+    databaseURL: 'https://storeapp-9941c-default-rtdb.firebaseio.com',
+    projectId: 'storeapp-9941c',
+    storageBucket: 'storeapp-9941c.appspot.com',
+    messagingSenderId: '405383394698',
+    appId: '1:405383394698:web:78ff125b31fdaf03f48473',
+    measurementId: 'G-NJHM89VBBF',
+  },
 };
 
 /*

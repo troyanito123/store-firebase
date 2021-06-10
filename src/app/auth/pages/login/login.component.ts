86be60a21618698a605af81111b9072b2eb269b2
jassmanni <jassmanniq@gmail.com>
2021-06-10 08:58:23 -0400

    login ready without loadings

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { UtilsService } from 'src/app/utils/utils.service';
+import { ValidatorService } from 'src/app/utils/validator.service';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +11,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private utilsService: UtilsService,
+    private validatorService: ValidatorService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createloginForm();
+  }
 
+  login() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const { name, email, password } = this.loginForm.value;
+
+    this.authService
+      .login(email, password)
+      .then(() => {
+        this.loginForm.reset();
+        this.router.navigate(['/tabs/home']);
+      })
+      .catch(async (err) => {
+        const alert = await this.utilsService.createAlert(err.message);
+        this.loginForm.get('password').reset();
+        alert.present();
+      });
+  }
+
+  private createloginForm() {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorService.emailPattern),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  invalidField(field: string) {
+    return (
+      this.loginForm.get(field).invalid && this.loginForm.get(field).touched
+    );
+  }
+
+  get emailMessageError(): string {
+    const errors = this.loginForm.get('email').errors;
+    let message = '';
+    if (errors?.required) {
+      message = 'Email es obligatorio';
+    } else if (errors?.pattern) {
+      message = 'Tiene que ser un email valido';
+    }
+    return message;
+  }
+
+  get passwordMessageError(): string {
+    const errors = this.loginForm.get('password').errors;
+    let message = '';
+    if (errors?.required) {
+      message = 'Contraseña es obligatoria';
+    } else if (errors?.minlength) {
+      message = 'Contraseña debe contener minimo 6 caracters';
+    }
+    return message;
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,20 +5,27 @@ import firebase from 'firebase/app';
 
 export enum userRole {
   user = 'USER',
-  admin = 'ADMIN'
+  admin = 'ADMIN',
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private db: AngularFireDatabase, private auth: AngularFireAuth) {}
 
-  constructor(private db: AngularFireDatabase, private auth: AngularFireAuth) { }
+  register(name: string, email: string, password: string) {
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((resp) => {
+        const uid = resp.user.uid;
+        return this.db
+          .object(`users/${uid}`)
+          .set({ name, email, role: userRole.user, uid });
+      });
+  }
 
-  register(name: string, email: string, password: string){
-    return this.auth.createUserWithEmailAndPassword(email, password).then(resp => {
-      const uid = resp.user.uid;
-      return this.db.object(`users/${uid}`).set({name, email, role: userRole.user, uid})
-    });
+  login(email: string, password: string) {
+    return this.auth.signInWithEmailAndPassword(email, password);
   }
 }
